29c7eda445b51be548408e89bc5833c1cbdbdf49
Angel Robertov <gelerob@dir.bg>
2017-01-18 23:06:30 +0200

    Data type and variables exercises- 18.01.2017.

---
+++ DataTypes and Variables/TriplesOfLatinLetters/LatinLetters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        char a = (char)('a' + n);
+        for (char i = 'a'; i < a; i++)
+            for (char b = 'a'; b < a; b++)
+                for (char c = 'a'; c < a; c++)
+                    Console.WriteLine("{0}{1}{2}", i, b, c);
+
+    }
+}
---
+++ DataTypes and Variables/VolumeOfPiramid/Piramid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeOfPiramid
+{
+    class Piramid
+    {
+        static void Main(string[] args)
+        {
+            double piramidLength, piramidWidth,piramidHeight, piramidVolume = 0;
+            Console.Write("Length: ");
+            piramidLength = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            piramidWidth = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            piramidHeight = double.Parse(Console.ReadLine());
+            piramidVolume = (piramidLength * piramidWidth * piramidHeight) / 3;
+            Console.WriteLine("Pyramid Volume: {0:F2}", piramidVolume);
+
+        }
+    }
+}
