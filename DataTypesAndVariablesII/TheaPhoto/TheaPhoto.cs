d7cb3aba463c054c890dcd0a6954bddc7b9d4d20
Angel Robertov <gelerob@dir.bg>
2017-01-19 21:43:40 +0200

    Adding Exercises

--- DataTypesAndVariablesII/TheaPhoto/TheaPhoto.cs
+++ DataTypesAndVariablesII/TheaPhoto/TheaPhoto.cs
@@ -3,22 +3,22 @@ class Program
 {
     static void Main()
     {
-        int n = int.Parse(Console.ReadLine());//amaunt of picture
-        int filterTime = int.Parse(Console.ReadLine());//filter time of every single picture
-        int filterFactor = int.Parse(Console.ReadLine());//percent good picture
-        int uploadTime = int.Parse(Console.ReadLine());//the amount of time needed for every 
-                                                       //filtered picture to be uploaded to her storage
+        int n = int.Parse(Console.ReadLine());            //....amaunt of picture.............................//
+        int filterTime = int.Parse(Console.ReadLine());   //....filter time of every single picture...........//
+        int filterFactor = int.Parse(Console.ReadLine()); //....percent good picture..........................//
+        int uploadTime = int.Parse(Console.ReadLine());   /*....the amount of time needed for every ...........
+                                                          ....filtered picture to be uploaded to her storage  */
 
         filterTime *= n;
         int filteredPicture = (int)Math.Ceiling((double)(n * filterFactor) / 100);
         uploadTime *= filteredPicture;
         int sumTime = filterTime + uploadTime;
-        
+        int day = sumTime / 86400;
         int hours = sumTime / 3600;
         int minutes = sumTime % 3600;
         int secounds = minutes % 60;
         minutes /= 60;
-        Console.WriteLine("{0:00}:{1:00}:{2:00}",hours,minutes,secounds);
+        Console.WriteLine("{0:0}:{1:00}:{2:00}:{3:00}",day,hours,minutes,secounds);
 
     }
 }
