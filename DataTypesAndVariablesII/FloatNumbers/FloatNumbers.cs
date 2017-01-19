2bc7caff60ccb7d6ed6930426d351b0e936cdc5a
Angel Robertov <gelerob@dir.bg>
2017-01-19 19:37:17 +0200

    Data types and variables exercises - 19.01.2017

---
+++ DataTypesAndVariablesII/FloatNumbers/FloatNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatNumbers
+{
+    class FloatNumbers
+    {
+        static void Main(string[] args)
+        {
+            decimal var1 = 3.141592653589793238M;
+            double var2 = 1.60217657F;
+            decimal var3 = 7.8184261974584555216535342341M;
+
+            Console.WriteLine(var1);
+            Console.WriteLine(var2);
+            Console.WriteLine(var3);
+        }
+    }
+}
---
+++ DataTypesAndVariablesII/TheaPhoto/TheaPhoto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());//amaunt of picture
+        int filterTime = int.Parse(Console.ReadLine());//filter time of every single picture
+        int filterFactor = int.Parse(Console.ReadLine());//percent good picture
+        int uploadTime = int.Parse(Console.ReadLine());//the amount of time needed for every 
+                                                       //filtered picture to be uploaded to her storage
+
+        filterTime *= n;
+        int filteredPicture = (int)Math.Ceiling((double)(n * filterFactor) / 100);
+        uploadTime *= filteredPicture;
+        int sumTime = filterTime + uploadTime;
+        
+        int hours = sumTime / 3600;
+        int minutes = sumTime % 3600;
+        int secounds = minutes % 60;
+        minutes /= 60;
+        Console.WriteLine("{0:00}:{1:00}:{2:00}",hours,minutes,secounds);
+
+    }
+}
+
